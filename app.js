f1db787d65c7dfe300c9e7739bf8bf0df28f9b75
Anna Panteleeva <apanteleeva94@gmail.com>
2023-02-20 15:42:01 +0300

    fix: рефакторит роуты

--- app.js
+++ app.js
@@ -4,13 +4,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
-const userRouter = require('./routes/users');
-const movieRouter = require('./routes/movies');
-const auth = require('./middlewares/auth');
-const { createUser, login } = require('./controllers/users');
-const { validateSignIn, validateSignUp } = require('./middlewares/validate');
+const router = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const NotFoundError = require('./errors/NotFoundError');
 const { rootErrorHandler } = require('./middlewares/rootErrorHandler');
 
 const { PORT = 3000 } = process.env;
@@ -23,9 +18,9 @@ mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
 });
 
 app.use(helmet());
@@ -34,14 +29,7 @@ app.use(express.json());
 app.use(requestLogger);
 app.use(limiter);
 
-app.post('/signup', validateSignUp, createUser);
-app.post('/signin', validateSignIn, login);
-app.use(auth);
-app.use('/users', userRouter);
-app.use('/movies', movieRouter);
-app.use('*', (req, res, next) => {
-  next(new NotFoundError('Указанный путь не найден'));
-});
+app.use(router);
 app.use(errorLogger);
 app.use(rootErrorHandler);
 
